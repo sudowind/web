2a4ec4c1e6e650530797e5301d69c8946a19576b
sudowind <wfpkueecs@163.com>
2016-10-09 17:43:53 +0800

    添加左右导航栏，footer

---
+++ assets/js/load.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wind on 2016/10/9.
+ */
+$('#page_footer').load('../../../include/html/footer.html');
+$('#left_bar').load('../../../include/html/student/left_bar.html');
+$('#right_bar').load('../../../include/html/student/right_bar.html');
