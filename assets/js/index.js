72c99e8dceabffef5690ddf76db6111ae74daf15
yilong <289378134@qq.com>
2016-10-09 20:08:49 +0800

    修改index.html 加入了新的jqPaginator分页插件

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yilong on 2016/10/9.
+ */
+/*
+$('#id').jqPaginator({
+    totalPages: 100,
+    visiblePages: 10,
+    currentPage: 1,
+    onPageChange: function (num, type) {
+        $('#text').html('µ±Ç°µÚ' + num + 'Ò³');
+    }
+});*/
