9cf4f0a4fa4fa7ccf796006b82f74a6ae9b6cd9d
sudowind <wfpkueecs@163.com>
2016-10-10 19:38:26 +0800

    评论界面完成

--- assets/js/student/book.js
+++ assets/js/student/book.js
@@ -1,3 +1,8 @@
 /**
  * Created by wind on 2016/10/10.
  */
+function submit_comment() {
+    // alert($('#user_comment').val());
+    $('#myModal').modal('show');
+    setTimeout(function(){ $('#myModal').modal('hide'); }, 1000);
+}
