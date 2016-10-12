23d7ae534281689b58a6d536c7724edc710f4e6f
sudowind <wfpkueecs@163.com>
2016-10-12 19:15:00 +0800

    修改load调用方法，每个页面中显式调用load进行加载

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,15 +1,20 @@
 /**
  * Created by wind on 2016/10/9.
  */
-$('#page_footer').load('../../../include/html/footer.html');
-$('#left_bar').load('../../../include/html/student/left_bar.html', function() {
-    if (typeof left_bar_cb != 'undefined') {
-        left_bar_cb();
-    }
-});
 
-$('#right_bar').load('../../../include/html/student/right_bar.html', function() {
-    if (typeof right_bar_cb != 'undefined') {
-        right_bar_cb();
-    }
-});
+function load(src) {
+
+    $('#page_footer').load(src + 'footer.html');
+    $('#left_bar').load(src + 'student/left_bar.html', function() {
+        if (typeof left_bar_cb != 'undefined') {
+            left_bar_cb();
+        }
+    });
+
+    $('#right_bar').load(src + 'student/right_bar.html', function() {
+        if (typeof right_bar_cb != 'undefined') {
+            right_bar_cb();
+        }
+    });
+
+}
