767325138b67fabf7caa8cd4750d6c8d40858d2b
yilong <289378134@qq.com>
2016-10-12 19:37:43 +0800

    tasks界面的编写以及简单js交互功能的实现。

---
+++ assets/js/student/tasks/tasks.js
@@ -0,0 +1,26 @@
+/**
+ * Created by yilong on 2016/10/12.
+ */
+
+function left_bar_cb() {
+    $('#tasks_button').attr('class', 'side-button-selected left-side-button');
+}
+
+$(".book .sort .read").click(function(){
+    $(".book .sort .read").addClass("index");
+    $(".book .sort .reading").removeClass("index");
+    $(".list").hover(
+        function(){
+            $(this).find(".list-book .remove ").css({"display":"block"});
+        },
+        function(){
+            $(this).find(".list-book .remove ").css({"display":"none"});
+        }
+    )
+});
+$(".book .sort .reading").click(function(){
+    $(".book .sort .reading").addClass("index");
+    $(".book .sort .read").removeClass("index");
+    $(".list").unbind("mouseenter mouseleave");
+});
+
