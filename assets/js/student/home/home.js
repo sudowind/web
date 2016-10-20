e0772bb1ef73b8cc1b6c1e547ad5cb1e18a94582
yilong <289378134@qq.com>
2016-10-20 15:17:58 +0800

    主页编写完善、以及部分图片的添加。

--- assets/js/student/home/home.js
+++ assets/js/student/home/home.js
@@ -1,3 +1,100 @@
 /**
  * Created by yilong on 2016/10/18.
  */
+
+    //条形图
+var myChart = echarts.init(document.getElementById('main'));
+
+option = {
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['我的平均分', '班级平均分', '年级平均分']
+    },
+    toolbox: {
+        show: false,
+            feature: {
+                magicType: {show: true, type: ['line', 'bar']},
+                restore: {show: true},
+                saveAsImage: {show: true}
+            }
+    },
+    //calculable: true,
+    xAxis: [
+        {
+            type: 'value',
+            max: '100'
+        }
+    ],
+    yAxis: [
+        {
+            type: 'category',
+            data: ['信息提取', '意义构建', '语义推论', '组织概况', '评价赏析']
+        }
+    ],
+    series: [
+        {
+            name: '我的平均分',
+            type: 'bar',
+            data: [80, 80, 80, 80, 50],
+            itemStyle:{
+                normal:{
+                    color:['#febc3c']
+                }
+            }
+        },
+        {
+            name: '班级平均分',
+            type: 'bar',
+            data: [100, 90, 80, 70, 60],
+            itemStyle:{
+                normal:{
+                    color: ['#d8d8d8']
+                }
+            }
+        },
+        {
+            name: '年级平均分',
+            type: 'bar',
+            data: [80, 80, 80, 80, 80],
+            itemStyle: {
+                normal:{
+                    color:['#eb8155']
+                }
+            }
+        }
+    ]
+}
+// 为echarts对象加载数据
+myChart.setOption(option);
+
+
+//饼状图
+var bar = new ProgressBar.Circle('#progress_bar', {
+    color: '#fb9e1d',
+    strokeWidth: 12,
+    trailWidth: 8,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#fb9e1d', width: 8 },
+    to: { color: '#fb9e1d', width: 12 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('0%');
+        } else {
+            circle.setText(value + '%');
+        }
+
+    }
+});
+
+
