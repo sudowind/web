d70f41cfb238743e3cbce59ac1d6b86e3b87d3dc
yilong <289378134@qq.com>
2016-10-24 18:20:35 +0800

    新增图片

--- assets/js/student/tasks/reading.js
+++ assets/js/student/tasks/reading.js
@@ -1,3 +1,14 @@
 /**
  * Created by yilong on 2016/10/21.
  */
+$(".unopen").click(function(){
+    $(".section").css("display","block");
+    $(".cover").css("display","block");
+})
+$(".section-retract").click(function(){
+    $(".section").css("display","none");
+    $(".cover").css("display","none");
+})
+$(".title img").click(function(){
+    $(this).attr("src","../../../assets/img/student/tasks/label.png");
+})
