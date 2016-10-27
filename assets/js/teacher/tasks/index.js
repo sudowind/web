aee0c11483b90f02205845bf87bb93a8ccc24405
sudowind <wfpkueecs@163.com>
2016-10-27 12:48:28 +0800

    modal作为单独的插件可以随时调用

--- assets/js/teacher/tasks/index.js
+++ assets/js/teacher/tasks/index.js
@@ -4,8 +4,10 @@
 var circles = [];
 
 function init () {
-    // TODO 希望对每一个进度环分别进行初始化
-    // $('.progress-circle')
+    load_table_row('#row1');
+    load_table_row('#row2');
+    load_table_row('#row3');
+    load_table_row('#row4');
 }
 
 $('.option').click(function () {
@@ -13,58 +15,53 @@ $('.option').click(function () {
     $(this).addClass('index');
 });
 
-var bar = new ProgressBar.Circle('#progress_bar', {
-    color: '#fb9e1d',
-    // This has to be the same size as the maximum width to
-    // prevent clipping
-    strokeWidth: 12,
-    trailWidth: 8,
-    easing: 'easeInOut',
-    duration: 1400,
-    text: {
-        autoStyleContainer: false
-    },
-    from: { color: '#fb9e1d', width: 8 },
-    to: { color: '#fb9e1d', width: 12 },
-    // Set default step function for all animate calls
-    step: function(state, circle) {
-        circle.path.setAttribute('stroke', state.color);
-        circle.path.setAttribute('stroke-width', state.width);
+function load_table_row(row_selector) {
+    // 从服务器拿到数据之后，将数据填充成表格
+    var img_src = '../../../assets/img/1.png';
+    var img_name = '诗词里的科学';
+    // 应该是对每一行分别填充，每一行都有一个id，通过这个id进行操作
+    $(row_selector).load('../../../include/html/teacher/task_table_line.html', function() {
+        var bars = new Array(2);
+        for (var i = 0; i < bars.length; ++i) {
+            bars[i] = new ProgressBar.Circle(row_selector + ' td:nth-child(' + (i + 2).toString() + ') div', {
+                color: '#fb9e1d',
+                // This has to be the same size as the maximum width to
+                // prevent clipping
+                strokeWidth: 12,
+                trailWidth: 8,
+                easing: 'easeInOut',
+                duration: 1400,
+                text: {
+                    autoStyleContainer: false
+                },
+                from: { color: '#fb9e1d', width: 8 },
+                to: { color: '#fb9e1d', width: 12 },
+                // Set default step function for all animate calls
+                step: function(state, circle) {
+                    circle.path.setAttribute('stroke', state.color);
+                    circle.path.setAttribute('stroke-width', state.width);
 
-        var value = Math.round(circle.value() * 100);
-        if (value == 0) {
-            circle.setText('0%');
-        } else {
-            circle.setText(value + '%');
+                    var value = Math.round(circle.value() * 100);
+                    if (value == 0) {
+                        circle.setText('0%');
+                    } else {
+                        circle.setText(value + '%');
+                    }
+
+                }
+            });
+            bars[i].set(Math.random());
         }
+    });
+}
 
-    }
-});
+// 删除一本书
+function delete_book() {
+    var message = '将《诗词里的科学》从2015级3班、2015级4班的阅读任务中删除？';
+    my_tip.alert(message);
+}
 
-var bar1 = new ProgressBar.Circle('#progress_bar2', {
-    color: '#fb9e1d',
-    // This has to be the same size as the maximum width to
-    // prevent clipping
-    strokeWidth: 12,
-    trailWidth: 8,
-    easing: 'easeInOut',
-    duration: 1400,
-    text: {
-        autoStyleContainer: false
-    },
-    from: { color: '#fb9e1d', width: 8 },
-    to: { color: '#fb9e1d', width: 12 },
-    // Set default step function for all animate calls
-    step: function(state, circle) {
-        circle.path.setAttribute('stroke', state.color);
-        circle.path.setAttribute('stroke-width', state.width);
+// 查看书本详情
+function go_to_detail() {
 
-        var value = Math.round(circle.value() * 100);
-        if (value == 0) {
-            circle.setText('0%');
-        } else {
-            circle.setText(value + '%');
-        }
-
-    }
-});
+}
