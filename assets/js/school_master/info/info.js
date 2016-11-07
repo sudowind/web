321e3ea247ae762500d2aae4e35fb6cc106f19c0
yilong <289378134@qq.com>
2016-11-07 11:04:46 +0800

    校长端开始

---
+++ assets/js/school_master/info/info.js
@@ -0,0 +1,3 @@
+/**
+ * Created by yilong on 2016/11/7.
+ */
--- assets/js/school_master/management/management.js
+++ assets/js/school_master/management/management.js
@@ -2,3 +2,8 @@ $(".out").click(function(){
     $("#myModal").removeClass('modal fade in').addClass('modal fade');
     $("#myModal").css("display","none");
 })
+
+$('.content .class-name p span').click(function () {
+    $(this).siblings().removeClass('index');
+    $(this).addClass('index');
+});
