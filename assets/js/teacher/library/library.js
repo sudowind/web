6556fbc0522cbbf1bf7ad776a179837cf0ae09dc
yilong <289378134@qq.com>
2016-11-07 14:52:01 +0800

    修改了教师端的一些button

--- assets/js/teacher/library/library.js
+++ assets/js/teacher/library/library.js
@@ -1,15 +1,15 @@
 /**
- * Created by wind on 2016/10/31.
+ * Created by yilong on 2016/11/7.
  */
-// 左右导航栏的回调函数
-function left_bar_cb() {
-    $('#library_button').attr('class', 'side-button-selected left-side-button');
-}
 $(".book .grade span").click(function(){
     $(this).siblings().attr("class","");
     $(this).attr("class","index");
 })
 $(".book .sort span").click(function(){
+    $(this).siblings().attr("class","");
+    $(this).attr("class","index");
+})
+$(".book .read-lv .button span").click(function(){
     $(this).siblings().attr("class","");
     $(this).attr("class","index");
 })
--- assets/js/teacher/management/management.js
+++ assets/js/teacher/management/management.js
@@ -4,5 +4,9 @@
 $(".out").click(function(){
     $("#myModal").removeClass('modal fade in').addClass('modal fade');
     $("#myModal").css("display","none");
+})
 
+$(".content .class-name p span").click(function(){
+    $(this).siblings().attr("class","");
+    $(this).attr("class","index");
 })
