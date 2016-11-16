3e38c7be5a56a4abe7f3cf75089ce1ff664f9096
sudowind <wfpkueecs@163.com>
2016-11-16 15:53:26 +0800

    添加密码验证函数

--- assets/js/student/password/password.js
+++ assets/js/student/password/password.js
@@ -2,6 +2,45 @@
  * Created by yilong on 2016/10/14.
  */
 //正则验证表单格式提示
+
+function is_letter(val) {
+    return !!((val >= 65 && val <= 90) || (val >= 97 && val <= 122));
+}
+function is_digital(val) {
+    return !!((val >= 48 && val <= 57));
+}
+
+function verification(password) {
+    var has_letter = false;
+    var has_digital = false;
+    var has_punctuation = false;
+    var verification_count = 0;
+    for (var i = 0; i < password.length; ++i) {
+        var tmp_char = password[i];
+        var ascii_num = tmp_char.charCodeAt();
+        if (ascii_num < 127 && ascii_num > 32) {
+            if (is_letter(ascii_num))
+                has_letter = true;
+            else if (is_digital(ascii_num))
+                has_digital = true;
+            else
+                has_punctuation = true;
+        }
+        else {
+            return false;
+        }
+    }
+    if (has_letter)
+        verification_count += 1;
+    if (has_digital)
+        verification_count += 1;
+    if (has_punctuation)
+        verification_count += 1;
+
+    return verification_count >= 2;
+}
+
+
 $("#password-1").blur(function(){
     var oldPassword = $("#password-1").val()
     var reg = /^[0-9]*$/;
